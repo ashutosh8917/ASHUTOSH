d9f81ad3759960b01e644543708137e43b71f7d6
ashutosh33 <thakurvishnu333@gmail.com>
2024-01-07 18:14:19 +0530

    HelloWorldPrinter.class

---
+++ HelloWorldPrinter.java
@@ -0,0 +1,9 @@
+public class HelloWorldPrinter {
+    public static void main(String[] args) {
+        // Print "Hello World" 10 times
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello World");
+        }
+    }
+}
+
